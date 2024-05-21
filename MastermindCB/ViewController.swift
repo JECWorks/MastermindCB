629a85bf922ece2c1dc712c2895a67d553e2d7bb
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-05-21 12:44:24 -0400

    Initial commit for Mastermind (cows & bulls)

--- MastermindCB/ViewController.swift
+++ MastermindCB/ViewController.swift
@@ -7,8 +7,15 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet var tableView: NSTableView!
+    
+    @IBOutlet var guess: NSTextField!
+    
+    var answer = ""
+    var guesses = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +28,35 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func submitGuess(_ sender: Any) {
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
+    
+    func result (for guess: String) -> String {
+        return "Result"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        
+        if tableColumn?.title == "Guess" {
+            //this is the Guess column, show a previous guess
+            vw.textField?.stringValue = guesses[row]
+        } else {
+            //this is the Result column, call our new method
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
+    }
+    
 }
 
